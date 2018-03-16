f922577441184a9625efb9f6082379e2fbaa2bf9
maaaashin324 <mstk.shin@gmail.com>
2018-03-16 13:42:33 +0900

    Signature is implemented but get new error which says timestamp has expired

--- src/Result.js
+++ src/Result.js
@@ -3,8 +3,10 @@ import {
   Text,
   View,
   Button,
+  FlatList,
 } from 'react-native';
 import { CLOUDSIGHT } from 'react-native-dotenv';
+import generateUrl from '../util/index';
 
 export default class Result extends Component {
   constructor(props) {
@@ -13,7 +15,7 @@ export default class Result extends Component {
     const { params } = this.props.navigation.state;
     this.getAnalysisUrl = params ? params.url : 'Failure';
 
-    this.state = { result: null };
+    this.state = { item: [] };
   }
 
   async componentDidMount() {
@@ -24,16 +26,24 @@ export default class Result extends Component {
         'Cache-Control': 'no-cache',
       },
     })).json();
+
     console.log(analysisResult);
-    
-    this.setState({ result: analysisResult.name });
+
+    const amazonUrl = generateUrl(analysisResult.name);
+    console.log(amazonUrl);
+
+    const amazonResult = await (await fetch(amazonUrl)).text();
+    console.log(amazonResult);
   }
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>response: {this.state.result}</Text>
+        <Text>Hello</Text>
+        {/* <FlatList
+          data={[]}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+        /> */}
         <Button
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
